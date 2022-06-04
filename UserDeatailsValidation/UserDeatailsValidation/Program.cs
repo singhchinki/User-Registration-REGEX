38378ce43180a0c18dd602e17900f9c2558794a9
chinki kumari singh <singhchinkikumari.com>
2022-06-04 23:19:50 +0530

    [Chinki] check user Last name validation

--- UserDeatailsValidation/UserDeatailsValidation/Program.cs
+++ UserDeatailsValidation/UserDeatailsValidation/Program.cs
@@ -1,3 +1,5 @@
-﻿UserDeatailsValidation.RegexUserFirst_Name userFirst_Name = new UserDeatailsValidation.RegexUserFirst_Name();
+﻿UserDeatailsValidation.RegexUserDetails userFirst_Name = new UserDeatailsValidation.RegexUserDetails();
 userFirst_Name.FirstNameValidation("Chinki");
+UserDeatailsValidation.RegexUserDetails userLast_Name = new UserDeatailsValidation.RegexUserDetails();
+userLast_Name.LastNameValidation("Singh");
 
--- UserDeatailsValidation/UserDeatailsValidation/UserRegistrationDeatailsValidation.cs
+++ UserDeatailsValidation/UserDeatailsValidation/UserRegistrationDeatailsValidation.cs
@@ -7,19 +7,30 @@ using System.Text.RegularExpressions;
 
 namespace UserDeatailsValidation
 {
-    public class RegexUserFirst_Name
+    public class RegexUserDetails
     {
          const string NAME = "^[A-Z]{1}[a-z]{2,}$";
-         public void FirstNameValidation(string firstName)
+         public void FirstNameValidation(string FirstName)
          {
-            if (Regex.IsMatch(firstName, NAME))
+            if (Regex.IsMatch(FirstName, NAME))
             {
                Console.WriteLine("First name is valid. ");
                return;
             }
                Console.WriteLine("First name is not valid");
          }
+        public void LastNameValidation(string LastName)
+        {
+            if (Regex.IsMatch(LastName, NAME))
+            {
+                Console.WriteLine("Last name is valid. ");
+                return;
+            }
+            Console.WriteLine("Last name is not valid");
+        }
+
     }
+
     
 }
 
