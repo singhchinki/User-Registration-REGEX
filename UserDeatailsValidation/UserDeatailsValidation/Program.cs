c03e63fd3ddba912c781409a3cdf76b2b6109e32
chinki kumari singh <singhchinkikumari.com>
2022-06-05 12:21:22 +0530

    [Chinki] check user Email Id validation

--- UserDeatailsValidation/UserDeatailsValidation/Program.cs
+++ UserDeatailsValidation/UserDeatailsValidation/Program.cs
@@ -3,3 +3,8 @@ userFirst_Name.FirstNameValidation("Chinki");
 UserDeatailsValidation.RegexUserDetails userLast_Name = new UserDeatailsValidation.RegexUserDetails();
 userLast_Name.LastNameValidation("Singh");
 
+Console.WriteLine("Enter Email Id");
+string Email = Console.ReadLine();
+UserDeatailsValidation.RegexUserDetails Email_Id = new UserDeatailsValidation.RegexUserDetails();
+Email_Id.EmailValidation(Email);
+
--- UserDeatailsValidation/UserDeatailsValidation/UserRegistrationDeatailsValidation.cs
+++ UserDeatailsValidation/UserDeatailsValidation/UserRegistrationDeatailsValidation.cs
@@ -10,7 +10,9 @@ namespace UserDeatailsValidation
     public class RegexUserDetails
     {
          const string NAME = "^[A-Z]{1}[a-z]{2,}$";
-         public void FirstNameValidation(string FirstName)
+         const string EMAIL_ID = "^(abc).?[a-z]{3,}[@](bl).?(co).?[a-z]{2,}$";
+         const string Mobile_no = "^[0-9]{1,3}[ ][789][0-9]{9} $";
+        public void FirstNameValidation(string FirstName)
          {
             if (Regex.IsMatch(FirstName, NAME))
             {
@@ -28,7 +30,15 @@ namespace UserDeatailsValidation
             }
             Console.WriteLine("Last name is not valid");
         }
-
+        public void EmailValidation(string Email)
+        {
+            if (Regex.IsMatch(Email, EMAIL_ID))
+            {
+                Console.WriteLine("Eamil ID is valid. ");
+                return;
+            }
+            Console.WriteLine("Email ID is not valid");
+        }
     }
 
     
